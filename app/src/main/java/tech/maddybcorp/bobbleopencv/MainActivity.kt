04794d95cdecbb42b2633ba775d196a5ec4c2791
maddyb99 <43785086+maddyb99@users.noreply.github.com>
2021-02-22 10:22:51 +0530

    add image picker

--- app/src/main/java/tech/maddybcorp/bobbleopencv/MainActivity.kt
+++ app/src/main/java/tech/maddybcorp/bobbleopencv/MainActivity.kt
@@ -2,17 +2,39 @@ package tech.maddybcorp.bobbleopencv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
 import android.widget.TextView
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    private val pickImage = 100
+    private var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.imageView)
+        button = findViewById(R.id.buttonLoadPicture)
+        button.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+        }
 
         // Example of a call to a native method
         findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            imageView.setImageURI(imageUri)
+        }
+    }
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
