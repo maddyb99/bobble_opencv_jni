0fab78811aea9c0fadbe94ae2ed850e085c029e6
maddyb99 <43785086+maddyb99@users.noreply.github.com>
2021-02-22 22:33:34 +0530

    connect using JNI try parse uri

--- app/src/main/java/tech/maddybcorp/bobbleopencv/MainActivity.kt
+++ app/src/main/java/tech/maddybcorp/bobbleopencv/MainActivity.kt
@@ -1,7 +1,6 @@
 package tech.maddybcorp.bobbleopencv
 
-import android.content.ContentProvider
-import android.content.ContentResolver
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
@@ -12,26 +11,34 @@ import android.util.Log
 import android.widget.TextView
 import android.widget.Button
 import android.widget.ImageView
-import java.io.File
+import android.widget.VideoView
+import androidx.core.graphics.PathUtils
+import androidx.core.net.toFile
 
 class MainActivity : AppCompatActivity() {
     lateinit var imageView: ImageView
-    lateinit var button: Button
+    lateinit var videoView: VideoView
+    private lateinit var loadButton: Button
+    lateinit var convertButton: Button
     private val pickImage = 2
     private var imageUri: Uri?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         imageView = findViewById(R.id.imageView)
-        button = findViewById(R.id.buttonLoadPicture)
-        button.setOnClickListener {
+        videoView=findViewById(R.id.videoView)
+        loadButton = findViewById(R.id.buttonLoadPicture)
+        convertButton = findViewById(R.id.convertButton)
+        convertButton.isEnabled=false
+//        imageView.isEnabled=false
+//        videoView.isEnabled=false
+        loadButton.setOnClickListener {
             val gallery = Intent(Intent.ACTION_GET_CONTENT)
-            gallery.type = "image/webp"
+            gallery.type = "video/mp4"
             startActivityForResult(gallery, pickImage)
         }
-
         // Example of a call to a native method
-        findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
+        findViewById<TextView>(R.id.sample_text).text = stringFromJNI().javaClass.simpleName
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -39,10 +46,20 @@ class MainActivity : AppCompatActivity() {
         Log.d("${this::class.simpleName}", "RESULT: ${resultCode== RESULT_OK}")
         if (resultCode == RESULT_OK && requestCode == pickImage) {
             imageUri = data?.data
-//            Log.d("${this::class.simpleName}","IMAGE URI: $imageUri")
-//            Log.d("${this::class.simpleName}", "IMAGE URI PATH: ${imageUri?.path}")
-//            var file: File = File(imageUri?.path!!)
-
+//            var realPathUtil:RealPathUtil= RealPathUtil;
+//            var realPath: String? =realPathUtil.getRealPath(applicationContext,imageUri!!);
+            Log.d("${this::class.simpleName}", "PATH: ${imageUri.getFilePath(context = applicationContext)}")
+            WebPObject(imageUri?.path!!);
+            Log.d("${this::class.simpleName}", "TYPE: ${data?.resolveType(contentResolver)}")
+            if(data?.resolveType(contentResolver)=="video/mp4") {
+                videoView.isEnabled=true
+                videoView.setVideoURI(imageUri)
+                videoView.start()
+                videoView.setOnCompletionListener { videoView.start() }
+                convertButton.isEnabled=true
+            }
+            else{
+                imageView.isEnabled=true
             Log.d("${this::class.simpleName}", "ANDROID VERSION: ${android.os.Build.VERSION.SDK_INT}")
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
                 var source=ImageDecoder.createSource(contentResolver, imageUri!!)
@@ -50,8 +67,10 @@ class MainActivity : AppCompatActivity() {
                 imageView.setImageDrawable(drawable)
                 if(drawable is AnimatedImageDrawable)
                     drawable.start()
+                convertButton.isEnabled=true
             } else {
                 TODO("VERSION.SDK_INT < P")
+            }
             }
 //            imageView.setImageURI(imageUri)
         }
@@ -60,13 +79,18 @@ class MainActivity : AppCompatActivity() {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
+    external fun WebPObject(str:String):Unit
+    external fun stringFromJNI():String
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
-            System.loadLibrary("opencv_java4");
+            System.loadLibrary("opencv_java4")
+            System.loadLibrary("bobble_opencv")
         }
     }
+}
+fun Uri?.getFilePath(context: Context): String {
+    return this?.let { uri -> RealPathUtil.getRealPath(context, uri) ?: "" } ?: ""
 }
